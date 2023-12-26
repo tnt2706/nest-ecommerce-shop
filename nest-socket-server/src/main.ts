c2f877ccee0a58dd74cf17f9bab7df68648589ed
Tran Tinh <tinhtranngoc@itrvn.com>
2023-12-26 17:48:55 +0700

    Init repo nest-socket-server used gRPC

--- nest-socket-server/src/main.ts
+++ nest-socket-server/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+
 import { AppModule } from './app.module';
+import { GrpcConfig } from './configs/grpc.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const grpcConfig = new GrpcConfig({
+    packageName: 'socketio',
+    protoFile: 'socketio_server.proto',
+  });
+
+  console.log(grpcConfig.get());
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: grpcConfig.get(),
+    },
+  );
+
+  await app.listen();
 }
+
 bootstrap();
