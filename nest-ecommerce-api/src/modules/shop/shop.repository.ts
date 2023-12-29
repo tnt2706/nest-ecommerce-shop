9842cd34eff7e149d227b8d1a057b5dcfc0c9304
Tran Tinh <tinhtranngoc@itrvn.com>
2023-12-29 23:59:32 +0700

    refactor

--- nest-ecommerce-api/src/modules/shop/shop.repository.ts
+++ nest-ecommerce-api/src/modules/shop/shop.repository.ts
@@ -9,7 +9,7 @@ export class ShopRepository {
   constructor(@InjectModel('Shop') private shopModel: Model<Shop>) {}
 
   async findByUserEmail(email: string) {
-    const foundShop = this.shopModel.findOne({ email: email }).lean();
+    const foundShop = this.shopModel.findOne({ email }).lean();
     return foundShop ? foundShop : null;
   }
 
